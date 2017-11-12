49ecc19b5e403285b78813fd0f7115b508cad3ea
Vanya A. Sergeev <v@sergeev.io>
2017-11-12 16:33:12 -0600

    panels: fix sorting in token volume chart

--- docs/js/app/panels.js
+++ docs/js/app/panels.js
@@ -567,7 +567,7 @@ TokenVolumeChartPanel.prototype = derive(Panel, {
 
     /* Sort tokens by fiat volume */
     tokens.sort(function (a, b) {
-        return statistics['volume'].tokens[a].volumeFiat.lt(statistics['volume'].tokens[b].volumeFiat);
+        return statistics['volume'].tokens[a].volumeFiat.lt(statistics['volume'].tokens[b].volumeFiat) - 0.5;
     });
 
     var tokenNames = [];
