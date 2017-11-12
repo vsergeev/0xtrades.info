d3e2701e4b9c540bdfac47a1b2fb77d9852374af
Vanya A. Sergeev <v@sergeev.io>
2017-11-12 16:42:57 -0600

    panels: add y axis label to fee feeless chart

--- docs/js/app/panels.js
+++ docs/js/app/panels.js
@@ -758,7 +758,14 @@ FeeFeelessChartPanel.prototype = derive(Panel, {
 
     var chartConfig = {
       type: 'bar',
-      options: {responsive: true, tooltips: {callbacks: {label: CHART_DEFAULT_TOOLTIP_CALLBACK}}, legend: {display: false}},
+      options: {
+        responsive: true,
+        tooltips: {callbacks: {label: CHART_DEFAULT_TOOLTIP_CALLBACK}},
+        legend: {display: false},
+        scales: {
+          yAxes: [{scaleLabel: {display: true, labelString: 'Trades'}}],
+        },
+      },
       data: { datasets: [{ backgroundColor: CHART_DEFAULT_COLORS, tooltips: [] }] }
     };
     this._chart = new Chart(elem.find('canvas')[0].getContext('2d'), chartConfig);
