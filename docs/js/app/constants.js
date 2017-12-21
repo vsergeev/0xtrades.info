f3fd83e25e5decf9b6fbcb30ffd5d9fb504c106f
Vanya A. Sergeev <v@sergeev.io>
2017-12-21 00:37:04 -0600

    constants: pre-populate token infos with token registry

--- docs/js/app/constants.js
+++ docs/js/app/constants.js
@@ -51,21 +51,263 @@ var ZEROEX_RELAY_ADDRESSES = {
 
 /* Populated by model */
 var ZEROEX_TOKEN_INFOS = {
-    /* Pre-load some newer token infos */
-    "0x8f8221afbb33998d8584a2b05749ba73c37a938a": {
-      name: "Request",
-      symbol: "REQ",
+    /* Token infos from the registry */
+    "0xe41d2489571d322189246dafa5ebde1f4699f498": {
+      name: "0x Protocol Token",
+      symbol: "ZRX",
       decimals: 18,
     },
-    "0xd4fa1460f537bb9085d22c7bccb5dd450ef28e3a": {
-      name: "Populous Platform",
-      symbol: "PPT",
+    "0x2956356cd2a2bf3202f771f50d3d14a367b48070": {
+      name: "Ether Token",
+      symbol: "WETH",
+      decimals: 18,
+    },
+    "0xe94327d07fc17907b4db788e5adf2ed424addff6": {
+      name: "Augur Reputation Token",
+      symbol: "REP",
+      decimals: 18,
+    },
+    "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a": {
+      name: "Digix DAO Token",
+      symbol: "DGD",
+      decimals: 9,
+    },
+    "0xfa05a73ffe78ef8f1a739473e462c54bae6567d9": {
+      name: "Lunyr",
+      symbol: "LUN",
+      decimals: 18,
+    },
+    "0xc66ea802717bfb9833400264dd12c2bceaa34a6d": {
+      name: "MakerDAO",
+      symbol: "MKR",
+      decimals: 18,
+    },
+    "0xbeb9ef514a379b997e0798fdcc901ee474b6d9a1": {
+      name: "Melon Token",
+      symbol: "MLN",
+      decimals: 18,
+    },
+    "0x9a642d6b3368ddc662ca244badf32cda716005bc": {
+      name: "Qtum",
+      symbol: "QTUM",
+      decimals: 18,
+    },
+    "0xd26114cd6ee289accf82350c8d8487fedb8a0c07": {
+      name: "OmiseGO",
+      symbol: "OMG",
+      decimals: 18,
+    },
+    "0xb97048628db6b661d4c2aa833e95dbe1a905b280": {
+      name: "TenXPay",
+      symbol: "PAY",
+      decimals: 18,
+    },
+    "0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0": {
+      name: "Eos",
+      symbol: "EOS",
+      decimals: 18,
+    },
+    "0x888666ca69e0f178ded6d75b5726cee99a87d698": {
+      name: "Iconomi",
+      symbol: "ICN",
+      decimals: 18,
+    },
+    "0x744d70fdbe2ba4cf95131626614a1763df805b9e": {
+      name: "StatusNetwork",
+      symbol: "SNT",
+      decimals: 18,
+    },
+    "0x6810e776880c02933d47db1b9fc05908e5386b96": {
+      name: "Gnosis",
+      symbol: "GNO",
+      decimals: 18,
+    },
+    "0x0d8775f648430679a709e98d2b0cb6250d2887ef": {
+      name: "Basic Attention Token",
+      symbol: "BAT",
+      decimals: 18,
+    },
+    "0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac": {
+      name: "Storj",
+      symbol: "STORJ",
       decimals: 8,
+    },
+    "0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c": {
+      name: "Bancor",
+      symbol: "BNT",
+      decimals: 18,
+    },
+    "0x960b236a07cf122663c4303350609a66a7b288c0": {
+      name: "Aragon",
+      symbol: "ANT",
+      decimals: 18,
+    },
+    "0x0abdace70d3790235af448c88547603b945604ea": {
+      name: "district0x",
+      symbol: "DNT",
+      decimals: 18,
+    },
+    "0xaec2e87e0a235266d9c5adc9deb4b2e29b54d009": {
+      name: "SingularDTV",
+      symbol: "SNGLS",
+      decimals: 0,
+    },
+    "0x419d0d8bdd9af5e606ae2232ed285aff190e711b": {
+      name: "FunFair",
+      symbol: "FUN",
+      decimals: 8,
+    },
+    "0xaf30d2a7e90d7dc361c8c4585e9bb7d2f6f15bc7": {
+      name: "FirstBlood",
+      symbol: "1ST",
+      decimals: 18,
+    },
+    "0x08711d3b02c8758f2fb3ab4e80228418a7f8e39c": {
+      name: "Edgeless",
+      symbol: "EDG",
+      decimals: 0,
+    },
+    "0x5af2be193a6abca9c8817001f45744777db30756": {
+      name: "Bitquence",
+      symbol: "BQX",
+      decimals: 8,
+    },
+    "0x607f4c5bb672230e8672085532f7e901544a7375": {
+      name: "iExec",
+      symbol: "RLC",
+      decimals: 9,
+    },
+    "0x667088b212ce3d06a1b553a7221e1fd19000d9af": {
+      name: "Wings",
+      symbol: "WINGS",
+      decimals: 18,
+    },
+    "0x41e5560054824ea6b0732e656e3ad64e20e94e45": {
+      name: "Civic",
+      symbol: "CVC",
+      decimals: 8,
+    },
+    "0xb63b606ac810a52cca15e44bb630fd42d8d1d83d": {
+      name: "Monaco",
+      symbol: "MCO",
+      decimals: 8,
+    },
+    "0xf433089366899d83a9f26a773d59ec7ecf30355e": {
+      name: "Metal",
+      symbol: "MTL",
+      decimals: 8,
+    },
+    "0x12fef5e57bf45873cd9b62e9dbd7bfb99e32d73e": {
+      name: "Cofoundit",
+      symbol: "CFI",
+      decimals: 18,
+    },
+    "0xaaaf91d9b90df800df4f55c205fd6989c977e73a": {
+      name: "Monolith TKN",
+      symbol: "TKN",
+      decimals: 8,
+    },
+    "0xe7775a6e9bcf904eb39da2b68c5efb4f9360e08c": {
+      name: "Token-as-a-Service",
+      symbol: "TAAS",
+      decimals: 6,
+    },
+    "0x2e071d2966aa7d8decb1005885ba1977d6038a65": {
+      name: "DICE",
+      symbol: "ROL",
+      decimals: 16,
+    },
+    "0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b": {
+      name: "Trustcoin",
+      symbol: "TRST",
+      decimals: 6,
+    },
+    "0x1776e1f26f98b1a5df9cd347953a26dd3cb46671": {
+      name: "Numeraire",
+      symbol: "NMR",
+      decimals: 18,
+    },
+    "0x7c5a0ce9267ed19b22f8cae653f198e3e8daf098": {
+      name: "Santiment Network Token",
+      symbol: "SAN",
+      decimals: 18,
+    },
+    "0xdd974d5c2e2928dea5f71b9825b8b646686bd200": {
+      name: "Kyber Network Crystal",
+      symbol: "KNC",
+      decimals: 18,
+    },
+    "0x01afc37f4f85babc47c0e2d0eababc7fb49793c8": {
+      name: "Wrapped Golem Network Token",
+      symbol: "WGNT",
+      decimals: 18,
+    },
+    "0xd0d6d6c5fe4a677d343cc433536bb717bae167dd": {
+      name: "adToken",
+      symbol: "ADT",
+      decimals: 9,
     },
     "0xab16e0d25c06cb376259cc18c1de4aca57605589": {
       name: "FinallyUsableCryptoKarma",
       symbol: "FUCK",
       decimals: 4,
+    },
+    "0x701c244b988a513c945973defa05de933b23fe1d": {
+      name: "openANX",
+      symbol: "OAX",
+      decimals: 18,
+    },
+    "0x514910771af9ca656af840dff83e8264ecf986ca": {
+      name: "ChainLink",
+      symbol: "LINK",
+      decimals: 18,
+    },
+    "0x8f8221afbb33998d8584a2b05749ba73c37a938a": {
+      name: "Request Network",
+      symbol: "REQ",
+      decimals: 18,
+    },
+    "0x27054b13b1b798b345b591a4d22e6562d47ea75a": {
+      name: "AirSwap",
+      symbol: "AST",
+      decimals: 4,
+    },
+    "0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4": {
+      name: "Enigma",
+      symbol: "ENG",
+      decimals: 8,
+    },
+    "0x818fc6c2ec5986bc6e2cbf00939d90556ab12ce5": {
+      name: "Kin",
+      symbol: "KIN",
+      decimals: 18,
+    },
+    "0x27dce1ec4d3f72c3e457cc50354f1f975ddef488": {
+      name: "AirToken",
+      symbol: "AIR",
+      decimals: 8,
+    },
+    "0x12480e24eb5bec1a9d4369cab6a80cad3c0a377a": {
+      name: "Substratum",
+      symbol: "SUB",
+      decimals: 2,
+    },
+    "0xb7cb1c96db6b22b0d3d9536e0108d062bd488f74": {
+      name: "Walton",
+      symbol: "WTC",
+      decimals: 18,
+    },
+    "0x4156d3342d5c385a87d264f90653733592000581": {
+      name: "Salt",
+      symbol: "SALT",
+      decimals: 8,
+    },
+
+    /* Token infos not yet in the registry */
+    "0xd4fa1460f537bb9085d22c7bccb5dd450ef28e3a": {
+      name: "Populous Platform",
+      symbol: "PPT",
+      decimals: 8,
     },
     "0x0e0989b1f9b8a38983c2ba8053269ca62ec9b195": {
       name: "Po.et",
